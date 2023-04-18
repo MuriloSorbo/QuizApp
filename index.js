f8298c6dc05aec37a2b26481af52eab2728071aa
Murilo Sorbo <developermurilo@gmail.com>
2023-04-17 22:17:01 -0300

    Created login page and it's route

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 const srcReader = require('./srcReader');
 
 const quizRouter = require('./Routes/quiz');
+const loginRouter = require('./Routes/login');
+
 const app = express();
 const port = 3000;
 
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/Pages/Quiz/Public', express.static('Pages/Quiz/Public'));
+app.use('/Pages/Login/Public', express.static('Pages/Login/Public'));
 
 app.use('/quiz', quizRouter);
+app.use('/login', loginRouter);
 
 const obj = srcReader.read();
 console.log(obj[0]);
