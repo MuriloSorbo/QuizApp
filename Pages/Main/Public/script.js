be29cc58795754cc2fe1d075858e0b9845fa9493
Murilo Sorbo <developermurilo@gmail.com>
2023-04-22 23:26:32 -0300

    Some changes for code optimization

--- Pages/Main/Public/script.js
+++ Pages/Main/Public/script.js
@@ -1,12 +1,22 @@
-function getUsers() {
-  const url = '/users';
-
+function getRequest(url, funcOk, funcErr) {
   const request = new XMLHttpRequest();
   request.open('GET', url);
   request.addEventListener('load', function () {
-    if (this.status == 200) handleData(this.responseText);
+    if (this.status == 200) funcOk(this.responseText);
+    else funcErr(this.responseText);
   });
   request.send();
+}
+
+function getTitle() {
+  getRequest(
+    '/title',
+    (response) => (document.getElementById('title').innerHTML = response)
+  );
+}
+
+function getUsers() {
+  getRequest('/users', (response) => handleData(response));
 }
 
 function handleData(data) {
@@ -24,3 +34,4 @@ function handleData(data) {
 }
 
 setInterval(getUsers, 1000);
+getTitle();
